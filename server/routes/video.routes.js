7f629c7eb8ae8a48ff394ddaf2fb6e98c4f8d968
Fritz Meyer <fritz.meyer@code.berlin>
2023-11-05 16:40:37 +0100

    deleted all files

--- server/routes/video.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const videoControllers = require("../controllers/videoControllers")
-
-router.get('/get-token-mod', videoControllers.getTokenMod);
-
-router.get('/get-token', videoControllers.getToken)
-
-module.exports = router;
