b572fae98a1447d273df5cb7dfcc87f6438d83cb
John <jayblez@gmail.com>
2023-11-17 11:49:42 +0100

    added a removerUser controller to groupMembershipControllers, slightly adjusted the leave/join controllers as well to flow better

--- server/controllers/groupMembershipControllers.js
+++ server/controllers/groupMembershipControllers.js
@@ -2,61 +2,98 @@ const { Group } = require("../models/Group.model");
 const { User } = require("../models/User.model");
 
 exports.join = async (req, res) => {
-    const {
-      body: { currentUserId },
-      params: { groupId },
-    } = req;
-  
-    try {
-      let group = await Group.findOne({ _id: groupId });
-      if (!group)
-        return res.status(400).send({ message: "Die Gruppe existiert nicht" });
-  
-      if (!group.users.includes(currentUserId)) {
-        await User.updateOne(
-          { _id: currentUserId },
-          { $push: { joinedGroups: group._id, meetings: group.meeting } }
-        );
-  
-        await Group.updateOne(
-          { _id: groupId },
-          { $push: { users: currentUserId } }
-        );
-  
-        res.status(200).send({ message: "Gruppe erfolgreich beigetreten" });
-      } else {
-        res.status(400).send({ message: "Sie sind bereits in dieser Gruppe" });
-      }
-    } catch (error) {
-      res.status(500).send({ message: `${error}` });
-    }
-  };
-  
-  exports.leave = async (req, res) => {
-    const {
-      body: { currentUserId },
-      params: { groupId },
-    } = req;
-  
-    try {
-      let group = await Group.findOne({ _id: groupId });
-      if (!group)
-        return res.status(400).send({ message: "Die Gruppe existiert nicht" });
+  const {
+    body: { currentUserId },
+    params: { groupId },
+  } = req;
 
-      let user = await User.findOne({ _id: currentUserId });
-      if (!user.joinedGroups.includes(groupId)) return res.status(400).send({ message: "Sie sind nicht in dieser Gruppe" });
-  
-      await Group.updateOne(
-        { _id: groupId },
-        { $pull: { users: currentUserId } }
-      );
-  
-      await User.updateOne(
-        { _id: currentUserId },
-        { $pull: { joinedGroups: groupId, meetings: group.meeting } }
-      );
-      res.status(200).send({ message: "Gruppe erfolgreich verlassen" });
-    } catch (error) {
-      res.status(500).send({ message: `${error}` });
-    }
-  };
+  try {
+    let group = await Group.findOne({ _id: groupId });
+    if (!group)
+      return res.status(400).send({ message: "Die Gruppe existiert nicht" });
+
+    if (group.users.includes(currentUserId))
+      return res
+        .status(400)
+        .send({ message: "Sie sind bereits Mitglied dieser Gruppe" });
+
+    await User.updateOne(
+      { _id: currentUserId },
+      { $push: { joinedGroups: group._id, meetings: group.meeting } }
+    );
+
+    await Group.updateOne(
+      { _id: groupId },
+      { $push: { users: currentUserId } }
+    );
+
+    res.status(200).send({ message: "Gruppe erfolgreich beigetreten" });
+  } catch (error) {
+    res.status(500).send({ message: `${error}` });
+  }
+};
+
+exports.leave = async (req, res) => {
+  const {
+    body: { currentUserId },
+    params: { groupId },
+  } = req;
+
+  try {
+    const group = await Group.findOneAndUpdate(
+      { _id: groupId },
+      { $pull: { users: currentUserId } }
+    );
+    if (!group)
+      return res.status(400).send({ message: "Die Gruppe existiert nicht" });
+
+    let user = await User.findOne({ _id: currentUserId });
+    if (!user.joinedGroups.includes(groupId))
+      return res
+        .status(400)
+        .send({ message: "Sie sind kein Mitglied dieser Gruppe" });
+
+    await User.updateOne(
+      { _id: currentUserId },
+      { $pull: { joinedGroups: groupId, meetings: group.meeting } }
+    );
+    res.status(200).send({ message: "Gruppe erfolgreich verlassen" });
+  } catch (error) {
+    res.status(500).send({ message: `${error}` });
+  }
+};
+
+exports.removeMember = async (req, res) => {
+  const {
+    body: { userId },
+    params: { groupId },
+  } = req;
+
+  try {
+    const group = await Group.findOneAndUpdate(
+      { _id: groupId },
+      { $pull: { users: userId } }
+    );
+    if (!group)
+      return res.status(400).send({ message: "Die Gruppe existiert nicht" });
+
+    await User.updateOne(
+      { _id: userId },
+      { $pull: { joinedGroups: groupId, meetings: group.meeting } }
+    );
+    res.status(200).send({ message: "Gruppenmitglied erfolgreich entfernt" });
+  } catch (error) {
+    res.status(500).send({ message: `${error}` });
+  }
+};
+
+// exports.inviteMember = async (req, res) => {
+//   const {
+//     body: { userId },
+//     params: { groupId },
+//   } = req;
+//   try {
+//   } catch (error) {
+//     res.status(500).send({ message: `${error}` });
+//   }
+// };
--- server/routes/groupMembership.routes.js
+++ server/routes/groupMembership.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const groupMembershipControllers = require('../controllers/groupMembershipControllers');
-const { verifyJWT } = require("../middleware/jwt.middleware.js");
+const { verifyJWT, verifyCurrentUser } = require("../middleware/jwt.middleware.js");
 
 router.put("/:groupId/join", verifyJWT, groupMembershipControllers.join);
 
 router.put("/:groupId/leave", verifyJWT, groupMembershipControllers.leave);
+
+router.patch("/:groupId/removeMember", verifyJWT, verifyCurrentUser, groupMembershipControllers.removeMember);
 
 module.exports = router;
