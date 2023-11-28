a3c90967e6cf7bf500bad162c38fc14e6d65bebf
John <jayblez@gmail.com>
2023-11-28 16:50:34 +0100

    added group member authentication

--- middleware/userAuthentication.middleware.js
+++ middleware/userAuthentication.middleware.js
@@ -1,4 +1,7 @@
 const { Group } = require('../models/Group.model')
+const { Meeting } = require('../models/Meeting.model')
+const { User } = require('../models/User.model')
+
 const myCustomError = require('../utils/myCustomError')
 
 const verifyCurrentUser = async (req, res, next) => {
@@ -6,10 +9,28 @@ const verifyCurrentUser = async (req, res, next) => {
 		userId: currentUserId,
 		params: { userId: paramsUserId },
 	} = req
-	console.log(currentUserId)
+	// console.log(currentUserId)
 	if (!currentUserId || currentUserId !== paramsUserId){
 		return next(myCustomError('Unauthorized', 401)) 
 	}
+	next()
+}
+
+const verifyGroupMember = async (req, res, next) => {
+	const { params: meetingId, userId: currentUserId } = req
+	if (!meetingId) return res.sendStatus(401)
+
+	const meeting = await Meeting.findOne({ _id: meetingId.meetingId })
+	const group = await Group.findOne({ _id: meeting.groupId })
+	// const user = await User.findOne({ _id: currentUserId })
+
+	if (!group) next(myCustomError('Group not found', 400))
+
+	// console.log(group.users)
+	// console.log(user._id)
+	if (currentUserId != group.moderatorId && !group.users.includes(currentUserId))
+		next(myCustomError('Unauthorized', 401))
+
 	next()
 }
 
@@ -29,4 +50,5 @@ const verifyGroupModerator = async (req, res, next) => {
 module.exports = {
 	verifyCurrentUser,
 	verifyGroupModerator,
+	verifyGroupMember
 }
--- routes/meetingMembership.routes.js
+++ routes/meetingMembership.routes.js
@@ -2,10 +2,23 @@ const router = require('express').Router()
 
 const meetingMembershipControllers = require('../controllers/meetingMembershipControllers')
 
+const {
+	verifyGroupMember,
+} = require('../middleware/userAuthentication.middleware.js')
 const { validateAuthToken } = require('../middleware/auth.middleware')
 
-router.put('/:meetingId/joinMeeting', validateAuthToken, meetingMembershipControllers.joinMeeting)
+router.put(
+	'/:meetingId/joinMeeting',
+	validateAuthToken,
+	verifyGroupMember,
+	meetingMembershipControllers.joinMeeting
+)
 
-router.put('/:meetingId/leaveMeeting', validateAuthToken, meetingMembershipControllers.leaveMeeting)
+router.put(
+	'/:meetingId/leaveMeeting',
+	validateAuthToken,
+	verifyGroupMember,
+	meetingMembershipControllers.leaveMeeting
+)
 
 module.exports = router
