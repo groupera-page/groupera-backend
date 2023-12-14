71f706e3f424eaec9b730732045f5707b438613f
Fritz Meyer <fritz.meyer@code.berlin>
2023-12-14 14:45:01 +0100

    adjusted header policy

--- config/index.js
+++ config/index.js
@@ -21,22 +21,28 @@ module.exports = (app) => {
 		app.set('trust proxy', 1);
 	}
 
-	// controls a very specific header to pass headers from the frontend
-	const corsConfig = {
-		origin: ['http://localhost:8080', process.env.FRONTEND_BASE_URL],
-		credentials: true,
-	}
-
-	app.use(cors(corsConfig))
-
-
-	app.options('*', cors(corsConfig))
+	app.use(
+		cors({
+			origin: ['http://localhost:8080', process.env.FRONTEND_ORIGIN],
+			credentials: true,
+		})
+	);
 
 	app.use((req, res, next) => {
-		res.header(
-			'Access-Control-Allow-Headers',
-			'x-access-token, Origin, Content-Type, Accept, Authorization'
-		);
+		// Website you wish to allow to connect
+		res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_BASE_URL);
+
+		// Request methods you wish to allow
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+		// Request headers you wish to allow
+		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+		// Set to true if you need the website to include cookies in the requests sent
+		// to the API (e.g. in case you use sessions)
+		res.setHeader('Access-Control-Allow-Credentials', true);
+
+		// Pass to next layer of middleware
 		next();
 	});
 
