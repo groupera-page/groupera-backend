fa7f4854bbbb099e06fd823662f658be9cacf7a9
Fritz Meyer <fritz.meyer@code.berlin>
2023-12-14 14:57:25 +0100

    bugfix in env variable name used in header policy

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
 
 	app.use(
 		cors({
-			origin: ['http://localhost:8080', process.env.FRONTEND_ORIGIN],
+			origin: ['http://localhost:8080', process.env.FRONTEND_BASE_URL],
 			credentials: true,
 		})
 	);
