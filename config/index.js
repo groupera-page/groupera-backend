3273da68070cfbee81570ba290eae6833ab2a8e1
Fritz Meyer <fritz.meyer@code.berlin>
2023-12-14 14:54:17 +0100

    removed header policy

--- config/index.js
+++ config/index.js
@@ -28,23 +28,23 @@ module.exports = (app) => {
 		})
 	);
 
-	app.use((req, res, next) => {
-		// Website you wish to allow to connect
-		res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_BASE_URL);
-
-		// Request methods you wish to allow
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-		// Request headers you wish to allow
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		res.setHeader('Access-Control-Allow-Credentials', true);
-
-		// Pass to next layer of middleware
-		next();
-	});
+	// app.use((req, res, next) => {
+	// 	// Website you wish to allow to connect
+	// 	res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_BASE_URL);
+	//
+	// 	// Request methods you wish to allow
+	// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	//
+	// 	// Request headers you wish to allow
+	// 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	//
+	// 	// Set to true if you need the website to include cookies in the requests sent
+	// 	// to the API (e.g. in case you use sessions)
+	// 	res.setHeader('Access-Control-Allow-Credentials', true);
+	//
+	// 	// Pass to next layer of middleware
+	// 	next();
+	// });
 
 	// In development environment the app logs
 	app.use(logger('dev'))
