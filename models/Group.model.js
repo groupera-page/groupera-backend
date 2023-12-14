8f92ce3ded17e6be768f40e42fac93244e85ec22
Fritz Meyer <43126565+meyerfr@users.noreply.github.com>
2023-12-14 11:18:37 +0100

    update topic validation

--- models/Group.model.js
+++ models/Group.model.js
@@ -124,7 +124,7 @@ const groupEditSchema = {
 		}),
 	selfModerated: Joi.bool(),
 	// Should topic be editable anyway?
-	topic: Joi.string().valid(topicsArray.join(', ')).label('Topic'),
+	topic: Joi.string().valid(...topicsArray).label('Topic'),
 }
 
 module.exports = { Group, groupSchema, groupCreateSchema, groupEditSchema }
