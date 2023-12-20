87ad12496768b7a32043bdd3149c7fe0d131b3f9
John <jayblez@gmail.com>
2023-12-19 21:59:08 -0500

    ugh, the code I got from the docs was from v1, not v2. Changed the URLs and validation method  in videoControllers

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -33,7 +33,7 @@ exports.createMeeting = async (req, res, next) => {
 	const { firstMeeting } = req.body
 	const { token } = res.locals
 
-	const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings`
+	const url = `${process.env.VIDEOSDK_API_ENDPOINT}/v2/rooms`
 	const options = {
 		method: 'POST',
 		headers: { Authorization: token, 'Content-Type': 'application/json' },
@@ -59,11 +59,11 @@ exports.validateMeetingId = async (req, res, next) => {
 	const token = res.locals.token
 	const meetingId = res.locals.result.data.meetingId
 
-	const url = `${process.env.VIDEOSDK_API_ENDPOINT}/api/meetings/${meetingId}`
+	const url = `${process.env.VIDEOSDK_API_ENDPOINT}/v2/rooms/validate/${meetingId}`
 
 	const options = {
-		method: 'POST',
-		headers: { Authorization: token },
+		method: 'GET',
+		headers: { Authorization: token, 'Content-Type': 'application/json' },
 	}
 
 	const result = await axios(url, options)
