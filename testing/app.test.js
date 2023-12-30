1276fce67d99c6f839c643bd9ce3c49ce1617e72
John <jayblez@gmail.com>
2023-12-30 14:34:15 -0500

    Getting more indepth with it

--- testing/app.test.js
+++ testing/app.test.js
@@ -4,11 +4,19 @@ const app = require('../app')
 describe('Post users', () => {
 	describe('Given a username and password', () => {
 		test('Should respond with 200 status code', async () => {
-			const response = await request(app).post('/signup').send({
-				username: 'username',
-				password: 'Password123',
+			const response = await request(app).post('/auth/signup').send({
+				dob: '2000-02-04',
+				alias: 'Fritz M.',
+				email: 'jayblez@gmail.com',
+				password: 'test-password',
+				gender: 'male',
+				terms: true,
+				questions: {
+					groupTheme: ['Depression', 'Generelle Hilfe'],
+					experience: 'never participated'
+				}
 			})
-			expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+			expect(response.headers['content-type']).toEqual(expect.stringContaining('json'))
 		})
 	})
 
