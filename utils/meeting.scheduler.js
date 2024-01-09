501f80fb1628b1da5833cd743c09981cc8100108
John Danaher <95925624+JohnDanaher@users.noreply.github.com>
2024-01-09 15:05:14 +0100

    Update meeting.scheduler.js

--- utils/meeting.scheduler.js
+++ utils/meeting.scheduler.js
@@ -7,39 +7,41 @@ const { getNextRecurrenceDate } = require('./meetingRecurrenceDate')
 process.env.TZ = 'Europe/Berlin'
 let currentDate = new Date()
 
-const meetingScheduler = schedule.scheduleJob('0 0 * * *', async () => {
+const meetingScheduler = schedule.scheduleJob('10 15 * * *', async () => {
 	try {
 		const groups = await findAllForEmails()
 
-		groups.forEach((group) => {
-			const nextMeeting = getNextRecurrenceDate(
-				group.meetings[0],
-				currentDate
-			)
+		const group = groups[19]
 
-			const reminder24Hours = new Date(nextMeeting)
-			reminder24Hours.setHours(reminder24Hours.getHours() - 24)
+		// groups.forEach((group) => {
+		const nextMeeting = getNextRecurrenceDate(
+			group.meetings[0],
+			currentDate
+		)
 
-			schedule.scheduleJob(reminder24Hours, async () => {
-				if (process.env.NODE_ENV === 'development') {
-					console.log('sending 24')
-				} else {
-					await sendMeetingReminder(
-						group.moderator.email,
-						group.moderator.alias,
-						group.name
-					)
-					if (group.members.length >= 1)
-						group.members.forEach(async (member) => {
-							await sendMeetingReminder(
-								member.email,
-								member.alias,
-								group.name
-							)
-						})
-				}
-			})
+		const reminder24Hours = new Date(nextMeeting)
+		reminder24Hours.setHours(reminder24Hours.getHours() - 24)
+
+		schedule.scheduleJob(reminder24Hours, async () => {
+			if (process.env.NODE_ENV === 'development') {
+				console.log('sending 24')
+			} else {
+				await sendMeetingReminder(
+					group.moderator.email,
+					group.moderator.alias,
+					group.name
+				)
+				if (group.members.length >= 1)
+					group.members.forEach(async (member) => {
+						await sendMeetingReminder(
+							member.email,
+							member.alias,
+							group.name
+						)
+					})
+			}
 		})
+		// })
 	} catch (error) {
 		console.error('Error fetching groups and meetings:', error)
 	}
